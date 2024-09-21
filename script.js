2fc2efb5bc04d09c11895f6794d5d67aceeeb622
daniel <shalomdaniel104@gamil.com>
2024-09-21 23:25:26 +0530

    all

---
+++ script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+const linkContainer = document.getElementById('protofilo');
+
+// Create a new anchor element
+const link = document.createElement('a');
+
+// Set the link's properties
+link.href = 'https://protoflio.com'; // Set the URL
+link.textContent = 'protoflio'; // Set the link text
+link.className = 'link'; // Add a class for styling
+
+// Append the link to the container
+linkContainer.appendChild(link);
+
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
